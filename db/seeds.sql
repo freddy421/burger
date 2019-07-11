37f17f8e80b02067e1e5205ecd3d6352d9063a41
freddy z <freddy421@gmail.com>
2019-07-11 02:08:19 -0500

    final

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,2 @@
 
-INSERT INTO burgers (burger_name) VALUES ('The Hulk');
-INSERT INTO burgers (burger_name) VALUES ('Meat Tornado');
-INSERT INTO burgers (burger_name) VALUES ('Whopper');
-INSERT INTO burgers (burger_name) VALUES ('Double Stack');
 INSERT INTO burgers (burger_name) VALUES ('Hamburger');
